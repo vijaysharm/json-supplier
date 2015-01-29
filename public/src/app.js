48c5c43771dba1f8cfc59a9653b14644ea15d007
Vijay Sharma <vijay.sharm@gmail.com>
2015-01-28 19:07:22 -0500

    Almost there

--- public/src/app.js
+++ public/src/app.js
@@ -1,28 +1,13 @@
-angular.module('RestProviderApp', ['ngRoute', 'ngResource', 'ngMessages'])
-	.config(function ($routeProvider) {
-	    $routeProvider
-	    	.when('/', {
-				templateUrl: 'templates/home.html',
-	        	controller: 'HomeController'
-	    	})
-	    	.otherwise({
-				redirectTo: '/'
-        	});
-	})
-	.factory('jsonParser', function () {
-		return {
-			isValid: function (data) {
-				try {
-					JSON.parse(data);
-					return true;
-				} catch (e) {
-					console.log('Failed to parse');
-					console.log(data);
-					console.log(e);
-					return false;
-				}				
+angular.module('RestProviderApp', ['schemaForm'])
+	.config(function () {
+		tv4.addFormat('my-json-format', function (data, schema) {
+			try {
+				JSON.parse(data);
+				return null;
+			} catch (e) {
+				return 'Invalid JSON';
 			}
-		}
+		});
 	})
 	.factory('restService', function ($http, $q) {
 		return {
@@ -39,7 +24,70 @@ angular.module('RestProviderApp', ['ngRoute', 'ngResource', 'ngMessages'])
 			}
 		};
 	})
-	.controller('HomeController', function ($scope, restService, jsonParser) {
+	.controller('HomeController', function ($scope, restService) {
+		$scope.form = [
+		{
+			"key": "action",
+			"type": "radios-inline",
+			"titleMap": [
+		    	{ "value": "GET", "name": "GET" },
+		    	{ "value": "POST", "name": "POST" },
+		    	{ "value": "PUT", "name": "PUT" }, 
+		    	{ "value": "DELETE", "name": "DELETE" }
+		    ]
+		}, 
+		"url",
+		{
+			"key": "code",
+			"feedback": false
+		}, 
+		{
+			"key": "headers",
+			"type": "textarea",
+		},
+		{
+			"key": "data",
+			"type": "textarea",
+		},
+		{
+			"type": "submit",
+			"style": "btn-info",
+			"title": "Save"
+		}];
+
+		$scope.schema = {
+			"type": "object",
+  			"properties": {
+				"action": {
+					"title": "JSON Action",
+					"type": "string",
+					"enum": ["GET", "POST", "PUT", "DELETE"],
+					"default": ["GET"]
+				},
+				"url": {
+					"title": "https://json-supplier.herokuapp.com/",
+					"type": "string"
+				},
+				"code": {
+					"title": "Response Code",
+					"type": "number",
+					"enum": [200, 404],
+					"default": [200]
+				},
+				"headers": {
+					"title": "Response Headers",
+					"type": "string",
+					"format": "my-json-format"
+				},
+				"data": {
+					"title": "JSON Data",
+					"type": "string",
+					"format": "my-json-format"
+				}
+  			},
+			"required": [ "action", "url", "code" ]
+		};
+
 		$scope.supplier = {
 			action: 'GET',
 			url: 'your/url/here',
@@ -47,57 +95,23 @@ angular.module('RestProviderApp', ['ngRoute', 'ngResource', 'ngMessages'])
 			code: 200,
 			headers: '{"content-type": "application/json"}'
 		};
-		$scope.save = function (supplier) {
-			if ( $scope.jsonForm.$invalid ) {
-				$scope.$broadcast('record:invalid');
-			} else {
+
+		$scope.onSubmit = function(form) {
+			$scope.$broadcast('schemaFormValidate');
+			if (! form.$valid) {
+				console.log("Form is invalid")
+				return;
 			}
 
-			if (jsonParser.isValid($scope.supplier.data) && 
-				jsonParser.isValid($scope.supplier.headers)) {
-				/*restService
-					.create(supplier)
-					.then(function (result) {
-						console.log("Success!");
-						console.log(result);
-					}, function (error) {
-						console.log("Fail!");
-						console.log(error);
-					});*/
-				console.log("Data is valid");
-			} else {
-				console.log("data is invalid");
-			}
-		};
-	})
-	.value('FieldTypes', {
-        text: ['Text', 'should be text'],
-        textarea: ['Textarea', 'should be text'],
-        number: ['Number', 'should be a number'],
-        datetime: ['Datetime', 'should be a datetime'],
-        password: ['Password', 'should be a password']
-    })
-	.directive('formField', function (FieldTypes) {
-		return {
-			restrict: 'EA',
-            templateUrl: 'templates/form-field.html',
-            replace: true,
-            scope: {
-                record: '=',
-                field: '@',
-                datatype: '@'
-            },
-            link: function ($scope, element, attr) {
-            	$scope.types = FieldTypes;
-                $scope.$on('record:invalid', function () {
-                    $scope[$scope.field].$setDirty();
-                });
-            }
-		};
-	})
-	.filter('labelCase', function () {
-        return function (input) {
-            input = input.replace(/([A-Z])/g, ' $1');
-            return input[0].toUpperCase() + input.slice(1);
-        };
-    });
+			console.log("form is valid");
+			restService
+				.create($scope.supplier)
+				.then(function (result) {
+					console.log("Success!");
+					console.log(result);
+				}, function (error) {
+					console.log("Fail!");
+					console.log(error);
+				});			
+		}		
+	});
